2f0b0d63f90cb446593fc1e61af0f58fc75c490f
amoskeyz <inspiron.amos@gmail.com>
2023-12-15 05:07:22 +0100

    design UI styles

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Icon from "../assets/images/icon.svg";
+import linkIcon from "../assets/images/linkIcon.svg";
+import copy from "../assets/images/copy.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div>
+        <p className="app-title">URL Shortner</p>
+        <div className="input_container">
+          <div className="input-wrap">
+            <img src={Icon} alt="icon" />
+            <input placeholder="https://pasteyoururl.com" />
+            <button className="shortn-btn">Shorten</button>
+          </div>
+        </div>
+        <div className="show-results">
+          <div className="single-result">
+            <div className="result-image">
+              <img src={linkIcon} alt="icon"/>
+            </div>
+            <div className="link">
+              <div>Zone Switch - Add Company Header Text</div>
+              <a href="https://mos.lv/zxskkwww" target="_blank" rel="noreferrer">
+                https://mos.lv/zxskkwww
+              </a>
+            </div>
+            <div className="copy">
+              <img src={copy} alt="icon"/>
+              <span>Copy Link</span>
+            </div>
+          </div>
+          <div className="single-result">
+            <div className="result-image">
+              <img src={linkIcon} alt="icon"/>
+            </div>
+            <div className="link">
+              <div>Zone Switch - Add Company Header Text</div>
+              <a href="https://mos.lv/zxskkwww" target="_blank" rel="noreferrer">
+                https://mos.lv/zxskkwww
+              </a>
+            </div>
+            <div className="copy">
+              <img src={copy} alt="icon"/>
+              <span>Copy Link</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
