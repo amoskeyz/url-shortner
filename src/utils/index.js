20e52374b57f5a3bac2b8f71f3f0a0c8e7c27b7b
amoskeyz <inspiron.amos@gmail.com>
2023-12-15 14:11:24 +0100

    add comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,28 +1,31 @@
-import { useRouteLoaderData } from "react-router-dom";
-
+//short Url code generator
 export const generateRandomString = () =>
   Math.random().toString(36).slice(2, 7);
 
 export const generateLink = (originalUrl) => {
   //get data if exist
   let data = localStorage.getItem("urlData");
-  const getCode = Math.random().toString(36).slice(2, 7);
+  const getCode = Math.random().toString(36).slice(2, 7); // genrate shortlink code
 
-  // console.log(data, "data")
+
   if (data) {
     data = JSON.parse(data);
-    const getLink = data.find((url) => url.originalUrl === originalUrl);
+    const getLink = data.find((url) => url.originalUrl === originalUrl); // check if url exists 
     if (getLink) {
       return {
         success: false,
       };
     }
+
+    // update array data
     data.push({
       id: data?.length + 1,
       originalUrl,
       shortUrl: `${window.origin}/${getCode}`,
       code: getCode,
     });
+
+    // push new data to local storage
     window.localStorage.setItem("urlData", JSON.stringify(data));
   } else {
     data = [
@@ -33,6 +36,8 @@ export const generateLink = (originalUrl) => {
         code: getCode,
       },
     ];
+
+    // push new data to local storage
     window.localStorage.setItem("urlData", JSON.stringify(data));
   }
 
@@ -42,8 +47,12 @@ export const generateLink = (originalUrl) => {
 export const deleteLink = (id) => {
   try {
     const data = JSON.parse(localStorage.getItem("urlData"));
+
+     // check if link exists
     const getlink = data.find((link) => link.id === id);
     if (getlink) {
+
+      //filter out link to be deleted from array
       window.localStorage.setItem(
         "urlData",
         JSON.stringify(data.filter((link) => link.id !== id))
@@ -60,15 +69,20 @@ export const editLink = ({ id, url }) => {
   try {
     let data = JSON.parse(localStorage.getItem("urlData"));
 
+    // check if link exists
     const getLink = data.find((item) => item.originalUrl === url);
-
-    console.log(getLink,);
     if (getLink) {
       return false
     }
+
+    //get link index in array
     const getlink = data.findIndex((link) => link.id === id);
     if (getlink > -1) {
+
+      //update original url
       data[getlink].originalUrl = url;
+
+      //push update link to local storage
       window.localStorage.setItem("urlData", JSON.stringify(data));
       return true;
     }
